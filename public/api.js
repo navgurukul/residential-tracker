c00047e819e4ee6aed5f78b15bbd69b03f1a17e9
Prabhatlodhi <lodhi.prabhat@gmail.com>
2024-11-29 18:16:49 +0530

    baseurl + hardcoded

--- public/api.js
+++ public/api.js
@@ -1,6 +1,6 @@
 
 const BaseUrl = import.meta.env.VITE_API_BASE_URL;
 
-const url =   BaseUrl
+const url =   BaseUrl || "https://script.google.com/macros/s/AKfycbyjEdP-0Q-vdtR8bU0wtgxghfqS_AVHc2dKRUTjjbuzLcdmt81f9lru5AnTF-B5gEum/exec"
 console.log(url,"fhgfhdgh")
 export default url;
